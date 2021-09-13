b05a01b43bbaba78d7f9353cd2a3fa5ec4bed422
Zenocolo <phzenhae@microsoft.com>
2021-09-13 21:10:40 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,23 @@
 variable "location" {
-  description = "Location of the network"
-  default     = "westeurope"
+  description = "Location of the Resource Group"
+}
+
+variable "rg-name" {
+  description = "Resource Group name"
 }
 
 variable "username" {
   description = "Username for Virtual Machines"
-  default     = "testadmin"
 }
 
 variable "password" {
   description = "Password for Virtual Machines"
-  default     = "Password1234!"
 }
 
 variable "vmsize" {
   description = "Size of the VMs"
-  default     = "Standard_DS1_v2"
 }
 
 variable "subnet_id" {
   description = "Reference to assigned subnet"
-  default     = "/subscriptions/65ed073b-97bd-4fb8-a098-f1a6aaeb32f9/resourceGroups/abb-800xa-europe-vnet-rg/providers/Microsoft.Network/virtualNetworks/ABB-SN-GLB-PROD-Z1A-VNET01/subnets/eu-project1"
 }
