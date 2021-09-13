dc6d36867781246b91429993880a6b1350895e6c
Zenocolo <phzenhae@microsoft.com>
2021-09-13 16:48:38 +0200

    Create vm.tf

---
+++ vm.tf
@@ -0,0 +1,46 @@
+resource "azurerm_network_interface" "eu-nic-project1" {
+  name                 = "nic-project1"
+  location             = data.azurerm_resource_group.project1.location
+  resource_group_name  = data.azurerm_resource_group.project1.name
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "ipconfig-project1"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "eu-vm-project1" {
+  name                  = "${local.prefix-eu}-vm-project1"
+  location              = data.azurerm_resource_group.project1.location
+  resource_group_name   = data.azurerm_resource_group.project1.name
+  network_interface_ids = [azurerm_network_interface.eu-nic-project1.id]
+  vm_size               = var.vmsize
+  delete_os_disk_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "myosdisk-project1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "${local.prefix-eu}-vm-project1"
+    admin_username = var.username
+    admin_password = var.password
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = local.tags_eu
